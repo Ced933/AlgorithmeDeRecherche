028bfd70a6714bb2a0a57b457007c6e1e321fb7f
Cédric <gloucedric@gmail.com>
2023-03-22 13:18:53 +0100

    input filtré

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ class App {
         // destructuring  pour avoir que le tableau des recette et non pas plusieur index 
         const { recipes } = await this.recetteApi.getApi();
 
-        // console.log(dataRecipes);
+        console.log(recipes);
         // boucler les ingrédients 
         // recipes[1].ingredients.map(ingr => {
         //     console.log(ingr.ingredient);
@@ -28,61 +28,34 @@ class App {
         const ulHtmlList = document.querySelector('.ul-container-list');
         function listOfIngredient() {
             // Boucle dans une boucle pour pouvoir maper les ingredients 
-            recipes.map(item => {
-                item.ingredients.map(item2 => {
-                    const liIngred = document.createElement('li');
-
-                    let allIngredient = item2.ingredient;
-                    let setIngredient = new Set();
-                    setIngredient.add(allIngredient);
-
-
-                    let text = "";
-                    setIngredient.forEach(function (value) {
-                        text += value;
-                    })
-                    // for (const x of gredientUnique.values()) {
-                    //     text += x;
-                    // }
-
-                    // console.log(text);
-
-                    liIngred.innerHTML = text;
-                    ulHtmlList.appendChild(liIngred);
-
-                })
+            // flatMap  sert a desctructurer les tableaux à l'interieur d'un tableau et les meme tous au meme niveau dans un tableau  
+            const ingredients = recipes.map(item => item.ingredients.map(ingredient => ingredient.ingredient.toLowerCase())).flatMap(x => x)
+            // je mets ingrédients dans set pour avoir chaque valeur une seul fois 
+            let setIngredient = new Set(ingredients);
+            // j'initialise une variable a vide 
+            let ingredientItem = "";
+            // je boucle le Set
+            setIngredient.forEach(function (value) {
+                ingredientItem += '<li>' + value + '</li>';
             });
+            // a l'intérieur du ul je mets tous mes li 
+            ulHtmlList.innerHTML = ingredientItem;
+            return setIngredient;
         }
         listOfIngredient();
 
         const ulHtmlListAppliance = document.querySelector('.ul-container-list-appliance');
         function listOfAppliance() {
             // Boucle dans une boucle pour pouvoir maper les ingredients 
-            recipes.map(item => {
+            const appliances = recipes.map(item => item.appliance.toLowerCase());
+            let setAppliance = new Set(appliances);
+            let appliancesItem = "";
 
-                const liIngred = document.createElement('li');
-
-                let allIngredient = item.appliance;
-                // console.log(allIngredient);
-                let setIngredient = new Set();
-                setIngredient.add(allIngredient);
-
-
-                let text = "";
-                // setIngredient.forEach(function (value) {
-                //     text += value;
-                // })
-                setIngredient.forEach(function (value) {
-                    text += value;
-                })
-
-                // console.log(x);
-
-                liIngred.innerHTML = text;
-                ulHtmlListAppliance.appendChild(liIngred);
-
-
+            setAppliance.forEach(function (value) {
+                appliancesItem += '<li>' + value + '</li>';
             });
+
+            ulHtmlListAppliance.innerHTML = appliancesItem;
         }
         listOfAppliance();
 
@@ -90,33 +63,17 @@ class App {
         const ulHtmlListUstensils = document.querySelector('.ul-container-list-ustensils');
         function listOfUstenciles() {
             // Boucle dans une boucle pour pouvoir maper les ingredients 
-            recipes.map(item => {
+            const ustensils = recipes.map(item => item.ustensils.map(item2 => item2.toLowerCase())).flatMap(x => x);
+            // const ustensils = recipes.ustensils.map(item => console.log(item.ustensils));
 
-                item.ustensils.map(itemUstensil => {
-                    // console.log(itemUstensil);
-                    const liIngred = document.createElement('li');
+            let setUstensils = new Set(ustensils);
+            let ustensilsItem = "";
 
-                    let allUstensils = itemUstensil;
-                    let setUstensils = new Set();
-                    setUstensils.add(allUstensils);
-
-                    let text = "";
-
-                    setUstensils.forEach(function (value) {
-                        text += value;
-                    })
-
-                    // for (const x of gredientUnique.values()) {
-                    //     text += x;
-                    // }
-
-
-
-                    liIngred.innerHTML = text;
-                    ulHtmlListUstensils.appendChild(liIngred);
-
-                })
+            setUstensils.forEach(function (value) {
+                ustensilsItem += '<li>' + value + '</li>';
             });
+
+            ulHtmlListUstensils.innerHTML = ustensilsItem;
         }
         listOfUstenciles();
 
@@ -125,12 +82,89 @@ class App {
 
 
         // toutes les recettes 
-        dataRecipes.recipes.forEach(recipe => {
+        // dataRecipes.recipes.forEach(recipe => {
 
-            const Template = new RecipesCard(recipe);
-            this.sectionRecipe.appendChild(Template.createRecipeCard());
+        //     const Template = new RecipesCard(recipe);
+        //     this.sectionRecipe.appendChild(Template.createRecipeCard());
 
-        });
+        // });
+        let figureSection = document.querySelector('.figure-section');
+        let mainInputSearch = document.querySelector('#search-main');
+
+        let dataArray;
+        async function getRecipes() {
+
+
+            createRecipesCard(recipes);
+        }
+        getRecipes();
+
+        console.log(dataRecipes);
+        function createRecipesCard(listRecipe) {
+
+            listRecipe.forEach(recipe => {
+
+                const recipeFigure = document.createElement('figure');
+                recipeFigure.classList.add('figure');
+                recipeFigure.innerHTML = ` 
+                <figcaption class="figcaption">
+                <div class="div-title-span">
+                    <h3>${recipe.name}</h3>
+                    <span><i class="fa-regular fa-clock me-2"></i>${recipe.time} min</span>
+                </div>
+                <div class="div-describe-p">
+                    <p class="p-ingredients">${recipe.ingredients.map(ingr => ingr.ingredient)} : ${recipe.ingredients.map(quanti => quanti.quantity)}</p>
+                    <p class="p-describe">${recipe.description}</p>
+                </div>
+            </figcaption>`
+                figureSection.appendChild(recipeFigure);
+            })
+
+        }
+
+
+        const ingredientInputSearch = document.querySelector('#search-ingredient');
+        ingredientInputSearch.addEventListener('input', filterIngredient)
+
+        function filterIngredient(e) {
+            ulHtmlList.innerHTML = "";
+            const searchStringIngredient = e.target.value.toLowerCase();
+
+            // const ingredientFilter = 
+
+
+        }
+
+
+
+
+
+
+
+        // a cahque fois qu'on ecrit a l'intérieur du input ça déclanche notre fonction filterData
+        mainInputSearch.addEventListener('input', filterData)
+
+        // function verifyLength(e) {
+
+        //     if (mainInputSearch.value.length > 2) {
+        //         const searchString = e.target.value.toLowerCase();
+        //         filterData();
+        //     }
+        // }
+
+        function filterData(e) {
+
+            figureSection.innerHTML = "";
+            // ce que je suis entrain de chercher dans l'input
+            const searchString = e.target.value.toLowerCase();
+            // création d'un tableau avec ma recherche actuelle 
+            const filterArr = recipes.filter(el =>
+                el.name.toLowerCase().includes(searchString))
+
+            createRecipesCard(filterArr)
+
+        }
+
 
 
 
